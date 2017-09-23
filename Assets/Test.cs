30ec24b6f53414f7639d38019a20d3c76df26360
RICK-WORKSTATIO\RickMa <979951208@qq.com>
2017-09-23 20:24:35 +0800

    init

---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public GameObject Cube;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Debug.Log("Press M button");
+            Cube.transform.Rotate(transform.up,15f);
+        }
+    }
+}
